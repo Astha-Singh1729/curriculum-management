1986bbed87eddd0931fef03def447e2a71c7daad
Astha Singh <asthasingh1729@gmail.com>
2022-11-22 01:50:35 +0530

    nhp

--- client/isdl/src/Login.js
+++ client/isdl/src/Login.js
@@ -1,18 +1,19 @@
 import React from 'react'
 import './Login.css'
-
+import { useStateValue } from './StateProvider';
 function Login() {
+  const [{ userType }, dispatch] = useStateValue();
   return (
     <div className='login'>
-        <div className='login__container'>
-      <h1>Login</h1>
-      <form>
-        <h5>Username</h5>
-        <input type='text'/>
-        <h5>Password</h5>
-        <input type='password' />
-        <button type='submit' className='login__signInButton'>Login</button>
-      </form>
+      <div className='login__container'>
+        <h1>Login</h1>
+        <form>
+          <h5>Username</h5>
+          <input type='text' />
+          <h5>Password</h5>
+          <input type='password' />
+          <button type='submit' className='login__signInButton'>Login</button>
+        </form>
       </div>
     </div>
   )
--- client/isdl/src/Start.js
+++ client/isdl/src/Start.js
@@ -1,23 +1,30 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import './Start.css';
-
+import { useStateValue } from './StateProvider';
 function Start() {
+
+  const [userType, dispatch] = useStateValue();
+  const handleClick = event => {
+    dispatch({
+      type: 'SET_ROLE',
+      role: event.currentTarget.id,
+    });
+  };
   return (
     <div className='start'>
-    <Link to='/login'>
-      <div className='start__option1'>
-        Student
-      </div>
-      <div className='start__option2'>
-        Faculty
-      </div>
-      <div className='start__option3'>
-        Admin
-      </div>
+      <Link to='/login'>
+        <div className='start__option1' id='Student' onClick={handleClick}>
+          Student
+        </div>
+        <div className='start__option2' id='Faculty' onClick={handleClick}>
+          Faculty
+        </div>
+        <div className='start__option3' id='Admin' onClick={handleClick}>
+          Admin
+        </div>
       </Link>
     </div>
   )
 }
-
 export default Start
